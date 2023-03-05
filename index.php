604943f78baa49371a777991b84154600ae83b02
Vanhalf <vh.win32@gmail.com>
2023-03-05 23:09:12 +0300

    add new carcase (cat)

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+
+
+$test = new \CowSay\Cat('Meow');
+$test->setEyes('oO');
+$test->setTongue('b');
+$test->setPoop('@@@');
+echo $test;
---
+++ src/Carcases/TraitsCAt.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace CowSay;
+
+
+use CowSay\Cat;
+
+class TraitsCaT extends Cat {
+
+	protected $eyes = 'oO';
+	protected $tongue = 'U';
+
+}
